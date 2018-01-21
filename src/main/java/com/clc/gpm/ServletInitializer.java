0ff0f756df23bc893d7f1b671c3730ba47873648
truongdxhaui <truonghauiclck9q@gmail.com>
2018-01-21 15:26:02 +0700

    Create Project

---
+++ src/main/java/com/clc/gpm/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.clc.gpm;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GpmApplication.class);
+	}
+
+}
